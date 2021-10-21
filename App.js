c1eb120a14c5a42678fafa5b3fddb81236dedeea
CajoAlbuquerque <carlosjo.da@gmail.com>
2021-10-21 16:11:19 +0100

    Add state to edit routine page

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
 
-import MainNavigator from './navigation/MainNavigator';
+import MainNavigator from "./navigation/MainNavigator";
+import store from "./redux/store";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-        <StatusBar style="auto" />
-        <MainNavigator/>
-    </NavigationContainer>
-  );
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <StatusBar style="auto" />
+                <MainNavigator />
+            </NavigationContainer>
+        </Provider>
+    );
 }
-
--- screens/EditRoutineScreen.js
+++ screens/EditRoutineScreen.js
@@ -1,19 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
     Text,
     StyleSheet,
     View,
     ScrollView,
     Image,
-    TouchableOpacity
-} from 'react-native';
+    TouchableOpacity,
+} from "react-native";
 import { useFonts } from "expo-font";
-import { Swipeable } from 'react-native-gesture-handler';
-import { Icon } from 'react-native-elements'
+import { Swipeable } from "react-native-gesture-handler";
+import { Icon } from "react-native-elements";
 
-import SettingsPagesHeader from '../components/SettingsPagesHeader';
-import PenIcon from '../assets/pen-icon.png'
+import SettingsPagesHeader from "../components/SettingsPagesHeader";
+import PenIcon from "../assets/pen-icon.png";
 import colors from "../config/colors";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    selectAfternoonHabits,
+    selectEveningHabits,
+    selectMorningHabits,
+    addMorningHabit,
+    removeMorningHabit,
+    addAfternoonHabit,
+    removeAfternoonHabit,
+    addEveningHabit,
+    removeEveningHabit,
+} from "../redux/habits";
+import allHabits from "../config/data";
+
+function HabitItem({ habitKey, deleteCallback }) {
+    const habitDescription = allHabits[habitKey].description;
+
+    return (
+        <View key={habitKey}>
+            <Swipeable
+                renderRightActions={() => leftAction(habitKey, deleteCallback)}
+            >
+                <View style={styles.habit}>
+                    <Text style={styles.habitText}>{habitDescription}</Text>
+                </View>
+            </Swipeable>
+            <View style={styles.division} />
+        </View>
+    );
+}
 
 export default function EditRoutineScreen({ navigation }) {
     let [fontsLoaded] = useFonts({
@@ -21,22 +51,31 @@ export default function EditRoutineScreen({ navigation }) {
         "Dosis-SemiBold": require("../assets/fonts/Dosis-SemiBold.ttf"),
     });
 
-    const [morningHabits, setMorningHabits] = useState(["Drink Water", "Yoga", "Plan Day"])
-    const [midDayHabits, setMidDayHabits] = useState(["Read a book", "Meditation", "Listen to podcasts"])
-    const [eveningHabits, setEveningHabits] = useState(["Cook Lunch", "Read News", "Drink tea"])
+    // const [morningHabits, setMorningHabits] = useState(["Drink Water", "Yoga", "Plan Day"])
+    // const [midDayHabits, setMidDayHabits] = useState(["Read a book", "Meditation", "Listen to podcasts"])
+    // const [eveningHabits, setEveningHabits] = useState(["Cook Lunch", "Read News", "Drink tea"])
 
-    const deleteHabit = (habit, timeOfDayHabits, setTimeoDayHabits) => {
-        let habitsCopy = timeOfDayHabits
-        let habitIndex = habitsCopy.indexOf(habit)
-        habitsCopy.splice(habitIndex, 1)
-        setTimeoDayHabits([...habitsCopy])
-    }
+    // State related variables
+    const morningHabits = useSelector(selectMorningHabits);
+    const afternoonHabits = useSelector(selectAfternoonHabits);
+    const eveningHabits = useSelector(selectEveningHabits);
+    const dispatch = useDispatch();
 
-    const leftAction = (habit, timeOfDayHabits, setTimeoDayHabits) => (
-        <TouchableOpacity style={styles.deleteAction} onPress={() => deleteHabit(habit, timeOfDayHabits, setTimeoDayHabits)}>
+    const deleteHabit = (habit, callback) => {
+        let habitsCopy = timeOfDayHabits;
+        let habitIndex = habitsCopy.indexOf(habit);
+        habitsCopy.splice(habitIndex, 1);
+        setTimeoDayHabits([...habitsCopy]);
+    };
+
+    const leftAction = (habitKey, deleteCallback) => (
+        <TouchableOpacity
+            style={styles.deleteAction}
+            onPress={() => dispatch(deleteCallback(habitKey))}
+        >
             <Text style={styles.deleteText}>Delete</Text>
         </TouchableOpacity>
-    )
+    );
 
     return (
         <View style={styles.container}>
@@ -46,108 +85,171 @@ export default function EditRoutineScreen({ navigation }) {
                     <View style={styles.timeOfDay}>
                         <View style={styles.timeOfDayRow}>
                             <Text style={styles.timeOfDayText}>MORNINGS</Text>
-                            <TouchableOpacity style={styles.timeOfDayRow} onPress={() => navigation.navigate('AddHabit', { chosenHabits: morningHabits })}>
+                            <TouchableOpacity
+                                style={styles.timeOfDayRow}
+                                onPress={() =>
+                                    navigation.navigate("AddHabit", {
+                                        chosenHabits: morningHabits,
+                                    })
+                                }
+                            >
                                 <Text style={styles.timeOfDayText}>Add</Text>
                                 <Icon
-                                    name='plus'
-                                    type='material-community'
+                                    name="plus"
+                                    type="material-community"
                                     color={colors.mute}
                                     size={22}
                                 />
                             </TouchableOpacity>
                         </View>
-                        {morningHabits.map(habit => (
-                            <View key={habit}>
-                                <Swipeable renderRightActions={() => leftAction(habit, morningHabits, setMorningHabits)}>
-                                    <View style={styles.habit}>
-                                        <Text style={styles.habitText}>{habit}</Text>
-                                    </View>
-                                </Swipeable>
-                                <View style={styles.division} />
-                            </View>
-                        ))}
+                        {morningHabits.map((habitKey) => {
+                            const habitDescription =
+                                allHabits[habitKey].description;
+
+                            return (
+                                <View key={habitKey}>
+                                    <Swipeable
+                                        renderRightActions={() =>
+                                            leftAction(
+                                                habitKey,
+                                                removeMorningHabit
+                                            )
+                                        }
+                                    >
+                                        <View style={styles.habit}>
+                                            <Text style={styles.habitText}>
+                                                {habitDescription}
+                                            </Text>
+                                        </View>
+                                    </Swipeable>
+                                    <View style={styles.division} />
+                                </View>
+                            );
+                        })}
                     </View>
                     <View style={styles.timeOfDay}>
                         <View style={styles.timeOfDayRow}>
                             <Text style={styles.timeOfDayText}>MID DAYS</Text>
-                            <TouchableOpacity style={styles.timeOfDayRow} onPress={() => navigation.navigate('AddHabit', { chosenHabits: midDayHabits })}>
+                            <TouchableOpacity
+                                style={styles.timeOfDayRow}
+                                onPress={() =>
+                                    navigation.navigate("AddHabit", {
+                                        chosenHabits: afternoonHabits,
+                                    })
+                                }
+                            >
                                 <Text style={styles.timeOfDayText}>Add</Text>
                                 <Icon
-                                    name='plus'
-                                    type='material-community'
+                                    name="plus"
+                                    type="material-community"
                                     color={colors.mute}
                                     size={22}
                                 />
                             </TouchableOpacity>
                         </View>
-                        {midDayHabits.map(habit => (
-                            <View key={habit}>
-                                <Swipeable renderRightActions={() => leftAction(habit, midDayHabits, setMidDayHabits)}>
-                                    <View style={styles.habit}>
-                                        <Text style={styles.habitText}>{habit}</Text>
-                                    </View>
-                                </Swipeable>
-                                <View style={styles.division} />
-                            </View>
-                        ))}
+                        {afternoonHabits.map((habitKey) => {
+                            const habitDescription =
+                                allHabits[habitKey].description;
+                            return (
+                                <View key={habitKey}>
+                                    <Swipeable
+                                        renderRightActions={() =>
+                                            leftAction(
+                                                habitKey,
+                                                removeAfternoonHabit
+                                            )
+                                        }
+                                    >
+                                        <View style={styles.habit}>
+                                            <Text style={styles.habitText}>
+                                                {habitDescription}
+                                            </Text>
+                                        </View>
+                                    </Swipeable>
+                                    <View style={styles.division} />
+                                </View>
+                            );
+                        })}
                     </View>
                     <View style={styles.timeOfDay}>
                         <View style={styles.timeOfDayRow}>
                             <Text style={styles.timeOfDayText}>EVENINGS</Text>
-                            <TouchableOpacity style={styles.timeOfDayRow} onPress={() => navigation.navigate('AddHabit', { chosenHabits: eveningHabits })}>
+                            <TouchableOpacity
+                                style={styles.timeOfDayRow}
+                                onPress={() =>
+                                    navigation.navigate("AddHabit", {
+                                        chosenHabits: eveningHabits,
+                                    })
+                                }
+                            >
                                 <Text style={styles.timeOfDayText}>Add</Text>
                                 <Icon
-                                    name='plus'
-                                    type='material-community'
+                                    name="plus"
+                                    type="material-community"
                                     color={colors.mute}
                                     size={22}
                                 />
                             </TouchableOpacity>
                         </View>
-                        {eveningHabits.map(habit => (
-                            <View key={habit}>
-                                <Swipeable renderRightActions={() => leftAction(habit, eveningHabits, setEveningHabits)}>
-                                    <View style={styles.habit}>
-                                        <Text style={styles.habitText}>{habit}</Text>
-                                    </View>
-                                </Swipeable>
-                                <View style={styles.division} />
-                            </View>
-                        ))}
+                        {eveningHabits.map((habitKey) => {
+                            const habitDescription =
+                                allHabits[habitKey].description;
+                            return (
+                                <View key={habitKey}>
+                                    <Swipeable
+                                        renderRightActions={() =>
+                                            leftAction(
+                                                habitKey,
+                                                removeEveningHabit
+                                            )
+                                        }
+                                    >
+                                        <View style={styles.habit}>
+                                            <Text style={styles.habitText}>
+                                                {habitDescription}
+                                            </Text>
+                                        </View>
+                                    </Swipeable>
+                                    <View style={styles.division} />
+                                </View>
+                            );
+                        })}
                     </View>
                     <View style={styles.bottomSection}>
-                        <Text style={styles.bottomText}>Swipe left to delete a habit</Text>
+                        <Text style={styles.bottomText}>
+                            Swipe left to delete a habit
+                        </Text>
                         <Image style={styles.penIcon} source={PenIcon} />
                     </View>
                 </View>
             </ScrollView>
-        </View >
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.white
+        backgroundColor: colors.white,
     },
     mainSection: {
         paddingHorizontal: 20,
         paddingBottom: 35,
-        paddingTop: 15
+        paddingTop: 15,
     },
     habit: {
         height: 46,
-        justifyContent: 'center'
+        justifyContent: "center",
     },
     habitText: {
         fontSize: 14,
-        fontFamily: 'Nunito-Regular',
+        fontFamily: "Nunito-Regular",
         color: colors.black,
     },
     timeOfDayRow: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
     },
     timeOfDay: {
         paddingTop: 5,
@@ -155,35 +257,35 @@ const styles = StyleSheet.create({
     },
     timeOfDayText: {
         fontSize: 13,
-        fontFamily: 'Nunito-Regular',
-        color: colors.mute
+        fontFamily: "Nunito-Regular",
+        color: colors.mute,
     },
     division: {
         borderBottomColor: colors.mute,
         borderBottomWidth: 1,
     },
     bottomSection: {
-        alignItems: 'center',
-        paddingTop: 40
+        alignItems: "center",
+        paddingTop: 40,
     },
     bottomText: {
         fontSize: 14,
-        fontFamily: 'Nunito-Regular',
+        fontFamily: "Nunito-Regular",
         color: colors.mute,
     },
     penIcon: {
         width: 64,
         height: 64,
-        marginVertical: 35
+        marginVertical: 35,
     },
     deleteAction: {
         backgroundColor: colors.red,
-        justifyContent: 'center'
+        justifyContent: "center",
     },
     deleteText: {
         fontSize: 14,
-        fontFamily: 'Nunito-Regular',
+        fontFamily: "Nunito-Regular",
         color: colors.white,
         paddingHorizontal: 15,
-    }
+    },
 });
